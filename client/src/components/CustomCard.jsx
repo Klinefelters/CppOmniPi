1163beafcab3c2c519c9c15c1296ec3854709760
klinefelters <stevenjk03@gmail.com>
2023-11-02 23:20:55 -0400

    I'm honestly just playing around rn

--- client/src/components/CustomCard.jsx
+++ client/src/components/CustomCard.jsx
@@ -1,24 +1,29 @@
-import {Card, CardHeader, CardBody, Flex, Spacer, IconButton, Image, Button, Heading} from "@chakra-ui/react"
-import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { NavLink } from "react-router-dom"
+import { Card, CardHeader, CardBody, Flex, Spacer, IconButton, Image, Heading, Spinner } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { NavLink } from "react-router-dom";
 
-export default function CustomCard({name, img, link}) {
-    return (
-        <Card borderRadius="2xl" bg="brand.grey">
-            <CardHeader bg="brand.600" borderRadius="2xl">
-                <Flex>
-                    <Heading color="white">{name}</Heading>
-                    <Spacer />
-                    <NavLink to={link}>
-                        <IconButton colorScheme="blue" icon={<ExternalLinkIcon/>}/>
-                    </NavLink>
-                </Flex>     
-            
-            </CardHeader>
+export default function CustomCard({ name, img, link, isLoading }) {
+  return (
+    <Card borderRadius="2xl" bg="brand.grey">
+      <CardHeader bg="brand.600" borderRadius="2xl">
+        <Flex>
+          <Heading color="white">{name}</Heading>
+          <Spacer />
+          <NavLink to={link}>
+            <IconButton colorScheme="blue" icon={<ExternalLinkIcon />} />
+          </NavLink>
+        </Flex>
+      </CardHeader>
 
-            <CardBody bg="brand.grey" borderRadius="2xl">
-                <Image src={img} w="480" h="360" alt="Image" borderRadius="2xl"/>
-            </CardBody>
-        </Card>
-    )
+      <CardBody bg="brand.grey" borderRadius="2xl">
+        {isLoading ? (
+          // Render a loading indicator (e.g., Spinner) when isLoading is true
+          <Spinner style={{ width: "300px", height: "300px"}} color="brand.600" />
+        ) : (
+          // Render the card content when isLoading is false
+          <Image src={img} w="480" h="360" alt="Image" borderRadius="2xl" />
+        )}
+      </CardBody>
+    </Card>
+  );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,51 @@
-import { Box, SimpleGrid} from "@chakra-ui/react"
-import CustomCard from "../components/CustomCard"
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import CustomCard from "../components/CustomCard";
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
+  const [isLoading, setIsLoading] = useState(true); // You can manage a global loading state for the entire dashboard
+
+  useEffect(() => {
+    // Simulate a delay for loading content (e.g., fetching data)
+    setTimeout(() => {
+      setIsLoading(false); // Set isLoading to false when content is ready
+    }, 2000); // You can adjust the delay as needed
+  }, []);
 
   return (
     <Box bg="black" h="100vh">
-      <SimpleGrid spacing={10} p="10" minChildWidth={400} w ="100hv" bg="black">
-        <CustomCard name="Controller" img="/img/Controller.png" link="controller"/>
-        <CustomCard name="Voice" img="/img/Voice.png" link="voice"/>
-        <CustomCard name="Video" img="/img/Video.png" link="video"/>
-        <CustomCard name="Touch" img="/img/Touch.png" link="touch"/>
-        <CustomCard name="Keyboard" img="/img/Keyboard.png" link="keyboard"/>
+      <SimpleGrid spacing={10} p="10" minChildWidth={400} w="100hv" bg="black">
+        <CustomCard
+          name="Controller"
+          img="/img/Controller.png"
+          link="controller"
+          isLoading={isLoading}
+        />
+        <CustomCard
+          name="Voice"
+          img="/img/Voice.png"
+          link="voice"
+          isLoading={isLoading}
+        />
+        <CustomCard
+          name="Video"
+          img="/img/Video.png"
+          link="video"
+          isLoading={isLoading}
+        />
+        <CustomCard
+          name="Touch"
+          img="/img/Touch.png"
+          link="touch"
+          isLoading={isLoading}
+        />
+        <CustomCard
+          name="Keyboard"
+          img="/img/Keyboard.png"
+          link="keyboard"
+          isLoading={isLoading}
+        />
       </SimpleGrid>
     </Box>
-  )
+  );
 }
