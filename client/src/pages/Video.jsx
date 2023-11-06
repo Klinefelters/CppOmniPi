e3f7cdc9ff5536c556eee6b0223fdc956da7c907
klinefelters <stevenjk03@gmail.com>
2023-11-06 14:20:45 -0500

    AI gesture - needs flipped

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,21 +1,86 @@
 import { Box, Flex, Spacer } from "@chakra-ui/react"
 import OmniPi from "../components/OmniPi"
-import { useState } from "react";
-import HandSegmentation from "../components/HandSegmentation";
+import { useState, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import {drawHand} from "../utilities/handPose"
 
 export default function Video() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  const [vx, setVX] = useState(0);
-  const [vy, setVY] = useState(0);
-  const [vr, setVR] = useState(0);
-  
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  runHandpose();
+
   return (
-    <Box bg="black" h="100vh">
-      <Flex>
-        <Spacer />
-          <HandSegmentation />
-        <Spacer />
-      </Flex>
+    <Box>
+      <Webcam
+        ref={webcamRef}
+//        videoConstraints={{facingMode: "user"}}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+
+      <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
     </Box>
-  )
+  );
 }
